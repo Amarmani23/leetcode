eff4fa49e26700753bbd3fbe815aed4e74c0c1b9
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-07-13 17:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,20 +9,47 @@ public:
 
     //     return max(incl,excl);
     // }
-    int solveMem(vector<int>&nums,vector<int>&dp,int n){
-        if(n<0) return 0;
-        if(n==0) return nums[0];
 
-        if(dp[n]!=-1){
-            return dp[n];
-        }
 
-        int incl=solveMem(nums,dp,n-2)+nums[n];
-        int excl=solveMem(nums,dp,n-1)+0;
+    // int solveMem(vector<int>&nums,vector<int>&dp,int n){
+    //     if(n<0) return 0;
+    //     if(n==0) return nums[0];
 
-        dp[n]= max(incl,excl);
-        return dp[n];
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+    //     int incl=solveMem(nums,dp,n-2)+nums[n];
+    //     int excl=solveMem(nums,dp,n-1)+0;
+
+    //     dp[n]= max(incl,excl);
+    //     return dp[n];
+    // }
+
+   int solveTab(vector<int>& nums) {
+    int n = nums.size();
+
+    if(n == 0)
+        return 0;
+
+    vector<int> dp(n, 0);
+
+    dp[0] = nums[0];
+
+    for(int i = 1; i < n; i++) {
+
+        int incl = nums[i];
+
+        if(i > 1)
+            incl += dp[i-2];
+
+        int excl = dp[i-1];
+
+        dp[i] = max(incl, excl);
     }
+
+    return dp[n-1];
+}
 
     int rob(vector<int>& nums) {
         // int n=nums.size();
@@ -30,9 +57,10 @@ public:
         // return ans;
 
         //recurssion + memorization(dp)
-        int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solveMem(nums,dp,n-1);
+        // int n=nums.size();
+        // vector<int>dp(n+1,-1);
+        // return solveMem(nums,dp,n-1);
         
+        return solveTab(nums);
     }
 };

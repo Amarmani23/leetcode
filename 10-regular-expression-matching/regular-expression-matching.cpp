6bb1f68538ad492eef72a72f76a390d8730acb29
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-07-24 22:39:36 +0530

    Time: 2 ms (58.79%) | Memory: 9.2 MB (57.60%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] represents if s[0..i-1] matches p[0..j-1]
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        // Base Case 1: Empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Base Case 2: Empty string matching patterns like "a*", "a*b*", "a*b*c*"
+        for (int j = 2; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } 
+                else if (p[j - 1] == '*') {
+                    // Option 1: Match 0 occurrences of p[j-2]
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // Option 2: Match 1 or more occurrences if character matches
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};

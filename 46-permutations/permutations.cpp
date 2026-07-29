0a736a09682e7191dd3797d8909d814c685a5d61
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-07-30 01:16:05 +0530

    Time: 3 ms (26.05%) | Memory: 10.6 MB (72.09%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        // vector<vector<int>> result;
+        // vector<int> curr;
+        // vector<bool> visited(nums.size(),false);
+        // backtrack( nums, result,curr,visited);
+        // return result;
+
+        //another method by swaping
+
         vector<vector<int>> result;
-        vector<int> curr;
-        vector<bool> visited(nums.size(),false);
-        backtrack( nums, result,curr,visited);
+        backtrack(nums,result,0);
         return result;
+        
     }
 private:
-    void backtrack(vector<int>& nums,vector<vector<int>> &result,vector<int> curr, vector<bool>& visited){
-        if(curr.size()==nums.size()){
-            result.push_back(curr);
+    // void backtrack(vector<int>& nums,vector<vector<int>> &result,vector<int> curr, vector<bool>& visited){
+    //     if(curr.size()==nums.size()){
+    //         result.push_back(curr);
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.size();++i){
+    //         if(visited[i]) continue;
+
+    //         visited[i]=true;
+    //         curr.push_back(nums[i]);
+    //         backtrack( nums, result,curr,visited);
+    //         curr.pop_back();
+    //         visited[i]=false;
+            
+    //     }
+    // }
+
+    void backtrack(vector<int>& nums,vector<vector<int>> &result,int index){
+        if(index==nums.size()){
+            result.push_back({nums});
             return;
         }
-        for(int i=0;i<nums.size();++i){
-            if(visited[i]) continue;
+        for(int i=index;i<nums.size();++i){
+        swap(nums[index],nums[i]);
+        backtrack(nums,result,index+1);
+        swap(nums[i],nums[index]);
 
-            visited[i]=true;
-            curr.push_back(nums[i]);
-            backtrack( nums, result,curr,visited);
-            curr.pop_back();
-            visited[i]=false;
-            
         }
     }
 };

bac03a9850b39fb58aeb8bb8cadf7617e2c64f5d
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-07-30 01:05:07 +0530

    Time: 1 ms (42.14%) | Memory: 12 MB (9.31%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -4,21 +4,21 @@ public:
         vector<vector<int>> result;
         vector<int> curr;
         vector<bool> visited(nums.size(),false);
-        backtrack( nums, result,curr,0,visited);
+        backtrack( nums, result,curr,visited);
         return result;
     }
 private:
-    void backtrack(vector<int>& nums,vector<vector<int>> &result,vector<int> curr,int index, vector<bool>& visited){
+    void backtrack(vector<int>& nums,vector<vector<int>> &result,vector<int> curr, vector<bool>& visited){
         if(curr.size()==nums.size()){
             result.push_back(curr);
             return;
         }
-        for(int i=index;i<nums.size();++i){
+        for(int i=0;i<nums.size();++i){
             if(visited[i]) continue;
 
             visited[i]=true;
             curr.push_back(nums[i]);
-            backtrack( nums, result,curr,0,visited);
+            backtrack( nums, result,curr,visited);
             curr.pop_back();
             visited[i]=false;
             

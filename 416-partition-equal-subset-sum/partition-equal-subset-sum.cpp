6f8ece303c1656ace24246d7e7e52fa7e39f2b20
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-21 23:24:00 +0530

    Time: 79 ms (82.84%) | Memory: 13.5 MB (86.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -31,44 +31,67 @@
 // };
 
 
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int total = 0;
+//         for (int num : nums) {
+//             total += num;
+//         }
+        
+//         // If the total sum is odd, we cannot split it into two equal integer subsets
+//         if (total & 1) return false; 
+        
+//         int target = total / 2;
+//         int n = nums.size();
+        
+//         // dp[i][j] stores whether a sum of j can be formed using the first i elements
+//         vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        
+//         // Base case: A sum of 0 can always be formed (by picking an empty subset)
+//         for (int i = 0; i <= n; i++) {
+//             dp[i][0] = true;
+//         }
+        
+//         // Fill the DP table iteratively
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= target; j++) {
+//                 // Option 1: Do not take the current element
+//                 bool notTake = dp[i - 1][j];
+                
+//                 // Option 2: Take the current element (if its value doesn't exceed the target sum j)
+//                 bool take = false;
+//                 if (nums[i - 1] <= j) {
+//                     take = dp[i - 1][j - nums[i - 1]];
+//                 }
+                
+//                 dp[i][j] = notTake || take;
+//             }
+//         }
+        
+//         return dp[n][target];
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int total = 0;
-        for (int num : nums) {
-            total += num;
-        }
-        
-        // If the total sum is odd, we cannot split it into two equal integer subsets
-        if (total & 1) return false; 
+        for (int num : nums) total += num;
+        if (total & 1) return false;
         
         int target = total / 2;
-        int n = nums.size();
+        vector<bool> dp(target + 1, false);
+        dp[0] = true; // Base case
         
-        // dp[i][j] stores whether a sum of j can be formed using the first i elements
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-        
-        // Base case: A sum of 0 can always be formed (by picking an empty subset)
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
-        
-        // Fill the DP table iteratively
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                // Option 1: Do not take the current element
-                bool notTake = dp[i - 1][j];
-                
-                // Option 2: Take the current element (if its value doesn't exceed the target sum j)
-                bool take = false;
-                if (nums[i - 1] <= j) {
-                    take = dp[i - 1][j - nums[i - 1]];
-                }
-                
-                dp[i][j] = notTake || take;
+        for (int num : nums) {
+            // Traverse backwards to prevent using the same element multiple times
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-        
-        return dp[n][target];
+        return dp[target];
     }
 };

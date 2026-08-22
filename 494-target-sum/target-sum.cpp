74bf4e1d0cb7c57eedd4f55dfa51f3355c0ab680
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-22 23:31:50 +0530

    Time: 14 ms (51.79%) | Memory: 25.9 MB (25.03%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,37 @@
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+        
+//         return rec(nums,0,0,target);
+//     }
+// private:
+//     int rec(vector<int>& nums,int sum,int idx,int &target){
+//         if(idx==nums.size()){
+//             return sum == target ? 1:0;
+//         }
+//         int pos=rec(nums,sum+nums[idx],idx+1,target);
+//         int neg = rec(nums,sum-nums[idx],idx+1,target);
+//         return pos+neg;
+//     }
+// };
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        return rec(nums,0,0,target);
+        int totalsum=0;
+        for(auto &s:nums) totalsum+=s;
+
+        vector<vector<int>>dp(nums.size(),vector<int>(2*totalsum+1,-1));
+        return rec(nums,0,0,target,totalsum,dp);
     }
 private:
-    int rec(vector<int>& nums,int sum,int idx,int &target){
+    int rec(vector<int>& nums,int sum,int idx,int &target,int offset,vector<vector<int>>&dp){
         if(idx==nums.size()){
             return sum == target ? 1:0;
         }
-        int pos=rec(nums,sum+nums[idx],idx+1,target);
-        int neg = rec(nums,sum-nums[idx],idx+1,target);
-        return pos+neg;
+        int colidx=sum+offset;
+        if(dp[idx][colidx]!=-1) return dp[idx][colidx];
+        int pos=rec(nums,sum+nums[idx],idx+1,target,offset,dp);
+        int neg = rec(nums,sum-nums[idx],idx+1,target,offset,dp);
+        return dp[idx][colidx]=pos+neg;
     }
 };

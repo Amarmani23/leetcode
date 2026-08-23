6f5fb9d193ed2ea11a10ac4c840ef5050a9d8b54
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-23 22:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.98%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,20 +1,38 @@
+// class Solution {
+// public:
+//     int minSteps(int n) {
+//         if(n==1) return 0;
+//         return 1+rec(1,1,n);
+//     }
+// private:
+//     int rec(int currlen,int cliplen,int n){
+//         if(currlen == n){
+//             return 0;
+//         }
+//         if(currlen>n) return 1e9;
+//         int pasteonly=1e9;
+//         if(cliplen>0){
+//             pasteonly=1+rec(currlen+cliplen,cliplen,n);
+//         }
+//         int copyAndPaste=2+rec(currlen+currlen,currlen,n);
+//         return min(pasteonly,copyAndPaste);
+//     }
+// };
+
+//by mathematically (it alway sum of its prime factor eg 6=2+3)
+
 class Solution {
 public:
     int minSteps(int n) {
-        if(n==1) return 0;
-        return 1+rec(1,1,n);
-    }
-private:
-    int rec(int currlen,int cliplen,int n){
-        if(currlen == n){
-            return 0;
+        int count=0;
+        int factor=2;
+        while(n>1){
+            while(n%factor==0){
+                count+=factor;
+                n/=factor;
+            }
+            factor++;
         }
-        if(currlen>n) return 1e9;
-        int pasteonly=1e9;
-        if(cliplen>0){
-            pasteonly=1+rec(currlen+cliplen,cliplen,n);
-        }
-        int copyAndPaste=2+rec(currlen+currlen,currlen,n);
-        return min(pasteonly,copyAndPaste);
+        return count;
     }
 };

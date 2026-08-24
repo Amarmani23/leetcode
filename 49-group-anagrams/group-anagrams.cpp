d5a4e2239b105c059aaea5776a942b7af9d54f78
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-24 23:43:15 +0530

    Time: 8 ms (96.34%) | Memory: 23.1 MB (98.37%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramMap;
+        
+        for (const string& str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            anagramMap[key].push_back(str);
+        }
+        
+        vector<vector<string>> result;
+        result.reserve(anagramMap.size());
+        
+        for (auto& pair : anagramMap) {
+            result.push_back(move(pair.second));
+        }
+        
+        return result;
+    }
+};

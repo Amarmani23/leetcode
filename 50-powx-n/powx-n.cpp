87030143a96cef4f01a2c5fb9f7b07ceceee84c8
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-26 00:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.55%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
+    // double myPow(double x, int n) {
+    //     double result=pow(x,n);
+    //     return result;
+    // }
+
+
     double myPow(double x, int n) {
-        double result=pow(x,n);
+        long long N = n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double result=1.0;
+        double curr_product=x;
+        while(N>0){
+            if(N & 1){
+                result*=curr_product;
+            }
+            curr_product*=curr_product;
+            N/=2;
+        }
         return result;
     }
 };

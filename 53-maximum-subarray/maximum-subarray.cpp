f6e3b89788e669f8a49d9e014f3d23caa1205e33
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-08-26 23:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.88%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int curr=0;
-        int maxi=INT_MIN;
-        for(int i =0 ;i<nums.size();i++){
-            curr+=nums[i];
-            maxi=max(maxi,curr);
-            if(curr<0){
-                curr=0;
-            }
+    // int maxSubArray(vector<int>& nums) {
+    //     int curr=0;
+    //     int maxi=INT_MIN;
+    //     for(int i =0 ;i<nums.size();i++){
+    //         curr+=nums[i];
+    //         maxi=max(maxi,curr);
+    //         if(curr<0){
+    //             curr=0;
+    //         }
             
+    //     }
+    //     return maxi;
+    // }
+
+    //standered kadane's algorithm
+    int maxSubArray(vector<int>& nums) {
+        int curr = nums[0];
+        int maxi = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            curr = max(nums[i], curr + nums[i]);
+            maxi = max(maxi, curr);
         }
+        
         return maxi;
     }
 };

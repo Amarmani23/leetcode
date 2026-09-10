5d602117ede989a014653d22f936ffdfc3598c59
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-09-10 22:41:36 +0530

    Time: 329 ms (14.47%) | Memory: 211.9 MB (8.13%) - LeetSync

---
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    long long mergeSort(vector<int>&pref,int start,int end){
+        long long count=0;
+        if(start<end){
+            int mid=start+(end-start)/2;
+            count+=mergeSort(pref,start,mid);
+            count+=mergeSort(pref,mid+1,end);
+            count+=merge(pref,start,mid,end);
+        }
+        return count;
+    }
+    long long merge(vector<int>&pref,int start,int mid,int end){
+        long long count=0;
+        vector<int>temp(end-start+1,0);
+        int i=start;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=end){
+            if(pref[i]<pref[j]){
+                count+=(end-j+1);
+                temp[k++]=pref[i++];
+            }else{
+                temp[k++]=pref[j++];
+            }
+        }
+        while(i<=mid) temp[k++]=pref[i++];
+        while(j<=end) temp[k++]=pref[j++];
+        for(int i = start;i<=end;i++){
+            pref[i]=temp[i-start];
+        }
+        return count;
+    }
+
+
+
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>pref(n+1,0);
+        for(int i =0;i<n;i++){
+            pref[i+1]=pref[i]+(nums[i] == target ? 1 : -1);
+        }
+        long long ans=mergeSort(pref,0,n);
+        return ans;
+    }
+};

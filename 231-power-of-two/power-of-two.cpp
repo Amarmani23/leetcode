8c8520719f787db568163165529cba71ad1950f2
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-09-14 22:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.78%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n<=0) return false;
+//         while(n%2==0){
+//             n/=2;
+//         }
+//         return n==1;
+//     }
+// };
+
+
+// by recursion -------------------
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
+    }
+};

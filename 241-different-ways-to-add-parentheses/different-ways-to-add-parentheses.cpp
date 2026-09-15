6bf8bb6bae8d054fe8d8c94aa4f292731f10a228
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-09-15 22:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (84.64%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,6 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> diffWaysToCompute(string expression) {
+//         vector<int>result;
+//         for(int i =0;i<expression.length();i++){
+//             char curr_char=expression[i];
+//             if(curr_char== '+'  || curr_char== '-' || curr_char == '*'){
+//                 string left_sub_expres=expression.substr(0,i);
+//                 string right_sub_expres=expression.substr(i+1);
+                
+//                 vector<int> left_result=diffWaysToCompute(left_sub_expres);
+//                 vector<int> right_result=diffWaysToCompute(right_sub_expres);
+
+//                 for(int left: left_result){
+//                     for(int right : right_result){
+//                         if(curr_char =='+'){
+//                             result.push_back(left+right);
+//                         }
+//                         else if(curr_char =='-'){
+//                             result.push_back(left-right);
+//                         }
+//                         else if(curr_char =='*'){
+//                             result.push_back(left*right);
+//                         }
+//                     }
+//                 }
+//             }
+
+//         }
+//         if(result.empty()){
+//             result.push_back(stoi(expression));
+
+//         }
+//         return result;
+//     }
+// };
+
+//memoization -----------------------------------
+
 class Solution {
+private:
+    unordered_map<string,vector<int>>memo;
 public:
     vector<int> diffWaysToCompute(string expression) {
+        if(memo.find(expression)!=memo.end()){
+            return memo[expression];
+        }
         vector<int>result;
         for(int i =0;i<expression.length();i++){
             char curr_char=expression[i];
@@ -31,6 +75,7 @@ public:
             result.push_back(stoi(expression));
 
         }
+        memo[expression]=result;
         return result;
     }
 };

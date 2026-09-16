10fc7da4ebcc8b5f2b24d0557c9e16454d7486c6
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-09-16 22:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (86.94%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
+
+// by recursion --------
+
+        // if(n<5) return 0;
+        // int ans=n/5;
+        // ans+=trailingZeroes(ans);
+        // return ans;
+
+
+// by iterative aproche
         if(n<5) return 0;
-        int ans=n/5;
-        ans+=trailingZeroes(ans);
-        return ans;
+        int count =0;
+        while(n>=5){
+            n/=5;
+            count+=n;
+        }
+        return count;
     }
 };

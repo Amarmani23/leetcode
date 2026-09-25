65aa4ff1a0a0600860b8e5a2beadb349ea6ee996
Amarmani yadav <amarmaniyadav9935@gmail.com>
2026-09-25 22:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (99.77%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -22,21 +22,38 @@ public:
 
 // ITERATIVE APPROCH
 //BFS
+    // int maxDepth(TreeNode* root){
+    //     if(!root) return 0;
+    //     int depth=0;
+    //     queue<TreeNode*>q;
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         depth++;
+    //         int levelsize=q.size();
+           
+    //         for(int i=0;i<levelsize;i++){
+    //             TreeNode* curr=q.front();
+    //             q.pop();
+    //             if(curr->left) q.push(curr->left);
+    //             if(curr->right) q.push(curr->right);
+    //         }
+    //     }
+    //     return depth;
+    // }
+
+//DFS
+
     int maxDepth(TreeNode* root){
         if(!root) return 0;
+        stack<pair<TreeNode*,int>>s;
+        s.push({root,1});
         int depth=0;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            depth++;
-            int levelsize=q.size();
-           
-            for(int i=0;i<levelsize;i++){
-                TreeNode* curr=q.front();
-                q.pop();
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
-            }
+        while(!s.empty()){
+            auto[curr,curr_depth]=s.top();
+            s.pop();
+            depth=max(curr_depth,depth);
+            if(curr->left) s.push({curr->left,curr_depth+1});
+            if(curr->right) s.push({curr->right,curr_depth+1});
         }
         return depth;
     }
